8fa1b1839fb635294bd6a06e43cced0e88c5d37e
4l3j4ndr0 <alejandro.casta3447@gmail.com>
2023-11-01 18:52:20 -0500

    :sparkles: Add new feature in lambda, PUT and POST events.

--- src/SecretsFunction/handler.py
+++ src/SecretsFunction/handler.py
@@ -16,7 +16,7 @@ def proccess_post_event(body):
     if body['type'] == 'login':
         tags = []
         for key, value in body.items():
-            if key != "password":
+            if key != "password" and key != "description":
                 tags.append({
                 "Key": key.upper(),
                 "Value": value
@@ -28,7 +28,27 @@ def proccess_post_event(body):
             Type='SecureString',
             Tags = tags
         )
-    return "Login created success."
+        message = "Login credentials created success."
+    return message
+
+def proccess_put_event(body):
+    if body['type'] == 'login':
+        tags = []
+        for key, value in body.items():
+            if key != "password" and key != "description" and key != "name":
+                tags.append({
+                "Key": key.upper(),
+                "Value": value
+            })
+        ssm_client.put_parameter(
+            Description = body["description"] if body['description'] != "" else body['name'],
+            Value = body["password"],
+            Type ='SecureString',
+            Tags = tags,
+            Overwrite = True
+        )
+        message = "Login credentials updated success."
+    return message
     
 
 def handler(event, context):
@@ -38,6 +58,9 @@ def handler(event, context):
         if method == 'POST':
             body = event['body'] if isinstance(event['body'], dict) and event['body'] is not None else json.loads(event['body'])
             data = proccess_post_event(body)
+        elif method == "PUT":
+            body = event['body'] if isinstance(event['body'], dict) and event['body'] is not None else json.loads(event['body'])
+            data = proccess_put_event(body)
         
         return build_response(200, data)
     except Exception as e:
